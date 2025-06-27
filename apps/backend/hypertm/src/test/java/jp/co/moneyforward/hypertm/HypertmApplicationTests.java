510ecda436adece57857b7ec3d965529dbd88ed6
Ryuichi Tokugami <tokugami.ryuichi@moneyforward.co.jp>
2025-06-27 15:16:59 +0900

    feat: set up backend with Spring Boot and Maven, including initial application structure and configuration

---
+++ apps/backend/hypertm/src/test/java/jp/co/moneyforward/hypertm/HypertmApplicationTests.java
@@ -0,0 +1,13 @@
+package jp.co.moneyforward.hypertm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HypertmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
