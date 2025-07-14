92c3c3a977f119c16682de0d2dfb821679941b31
Ryuichi Tokugami <tokugami.ryuichi@moneyforward.co.jp>
2025-07-14 11:27:12 +0900

    feat: Initialize Go backend with Gin framework
    - Added main.go to set up the Gin server with CORS configuration.
    - Introduced route handling for frontend requests in noroute.go.
    - Removed Maven-related files (mvnw, mvnw.cmd, pom.xml, and wrapper properties) as the project transitions to a Go-based backend.
    - Deleted Java Spring Boot application files (HypertmApplication.java, WebConfig.java, application.properties, and test classes) to fully migrate to Go.
    - Added go.sum for dependency management.

---
+++ apps/backend/main.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"log"
+	"time"
+
+	"github.com/gin-contrib/cors"
+	"github.com/gin-gonic/gin"
+	"jp.co.moneyforward/pf-skillsearch/route"
+)
+
+func main() {
+
+	log.Println("start server")
+
+	router := gin.Default()
+
+	router.MaxMultipartMemory = 768 << 20 // 768 MiB
+
+	// setting cors
+	router.Use(cors.New(cors.Config{
+		// would to allow all origins
+		AllowOrigins: []string{
+			"http://localhost:5173", // Vite development server
+			"*",
+		},
+		// allow GET, POST, PUT, DELETE, OPTIONS methods.
+		AllowMethods: []string{
+			"POST",
+			"GET",
+			"PUT",
+			"OPTIONS",
+			"DELETE",
+		},
+		// allow http request headers.
+		AllowHeaders: []string{
+			"Access-Control-Allow-Credentials",
+			"Access-Control-Allow-Headers",
+			"Content-Type",
+			"Content-Length",
+			"Accept-Encoding",
+			"Accept",
+			"Authorization",
+			"X-Ms-Client-Principal-Name",
+			"X-Ms-Token-Aad-Access-Token",
+		},
+		AllowCredentials: true,
+		MaxAge:           24 * time.Hour,
+	}))
+
+	// frontend hosting.
+	router.NoRoute(route.Frontend)
+
+	router.Run(":80")
+}
